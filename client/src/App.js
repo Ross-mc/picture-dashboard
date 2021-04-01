6525fc2491bb2ec527e92761d0be2cc94db9d3d5
Ross McIntyre <ross.mcintyre@bjss.com>
2021-04-01 15:10:21 +0100

    image changes on defined interval, currently hardcoded to every 15 seconds

--- client/src/App.js
+++ client/src/App.js
@@ -10,16 +10,23 @@ const App = () => {
   const [currentTime, setTime] = useState(undefined);
   const [currentImages, setImages] = useState([]);
   const [currentImage, setImage] = useState("");
-  const [photoInterval, setPhotoInterval] = useState(1)
+  const [photoInterval, setPhotoInterval] = useState(0.25);
+  const [changeImage, setChangeImage] = useState(false);
 
-
+  let timeSinceInterval = 0;
 
 
   const intervalEvery1000milli = () => {
     setInterval(() => {
+      if (timeSinceInterval > photoInterval * 60 * 1000) {
+        setChangeImage(!changeImage)
+        selectRandomImage();
+        timeSinceInterval = 0;
+      }
       const date = new Date();
       setDate(date.toDateString());
-      setTime(date.toLocaleTimeString())
+      setTime(date.toLocaleTimeString());
+      timeSinceInterval += 1000
     }
       , 1000)
   }
@@ -31,25 +38,22 @@ const App = () => {
   }
 
   useEffect(() => {
-    console.log(currentImage)
-
-  }, [currentImage])
+    selectRandomImage();
+  }, [changeImage])
 
 
   useEffect(() => {
     selectRandomImage();
-
   }, [currentImages])
 
 
   useEffect(async () => {
     const responseFromPexels = await API.getImages();
     setImages(responseFromPexels.data.photos);//hh
-    // selectRandomImage();
-    // intervalEvery1000milli();
-    // hello
+    intervalEvery1000milli();
 
   }, [])
+
   return (
     <div className="App">
       <Clock date={currentDate} time={currentTime} />
--- client/src/components/Background.js
+++ client/src/components/Background.js
@@ -1,7 +1,6 @@
 import React from "react";
 
 const Background = ({ currentImg }) => {
-  console.log(currentImg)
   return (
     <img className="bg-img" src={currentImg.src.original} alt="background image" />
   )
