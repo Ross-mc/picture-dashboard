0e2af805d49b2d65c70ab3fa1fdea6abd83b5c89
Ross McIntyre <rossmci90@hotmail.co.uk>
2021-04-07 09:21:03 +0100

    changed api call to return photos only (some CGI images were included prev)

--- client/src/App.js
+++ client/src/App.js
@@ -26,9 +26,9 @@ const App = () => {
       interval: 0.25,
       searchTerm: "landscape"
     }
-  })
+  });
 
-  const [currentDate, setDate] = useState(new Date().toDateString());
+  const [currentDate, setDate] = useState(new Date().toLocaleDateString());
   const [currentTime, setTime] = useState(new Date().toLocaleTimeString());
   const [currentImages, setImages] = useState([]);
   const [currentImage, setImage] = useState("");
@@ -46,51 +46,62 @@ const App = () => {
   };
 
   const toggleTaskbar = () => {
-    setTaskbar(!displayTaskbar)
-  }
+    setTaskbar(!displayTaskbar);
+  };
 
   useEffect(() => {
     selectRandomImage();
-  }, [currentImages])
+  }, [currentImages]);
 
   useEffect(() => {
     setTimeout(() => {
       setTimeSinceInterval(timeSinceInterval + 1000)
     }, 1000)
     if (timeSinceInterval % (settingsState.photo.interval * 60 * 1000) === 0) {
-      selectRandomImage()
+      selectRandomImage();
     }
-    const { timeDisplay } = settingsState
+    const { timeDisplay, dateDisplay } = settingsState;
 
     const timeOptions = {
       hour12: timeDisplay.hour12,
       hour: "2-digit",
       minute: "2-digit"
-    }
+    };
     if (timeDisplay.seconds) {
-      timeOptions.second = "2-digit"
+      timeOptions.second = "2-digit";
     }
+    const dateOptions = {
+      month: "long",
+      day: "numeric"
+    }
+    if (dateDisplay.day) {
+      dateOptions.weekday = "long"
+    };
+    if (dateDisplay.format === "long") {
+      dateOptions.year = "numeric"
+    };
+    // tolocaledatestring and localetimestring have two optional arguments. the first defines what locale to use (i.e. en-gb for uk or de-DE for germany) by passing
     const date = new Date();
-    setDate(date.toDateString());
-    setTime(date.toLocaleTimeString([], timeOptions));
+    setDate(date.toLocaleDateString(undefined, dateOptions));
+    setTime(date.toLocaleTimeString(undefined, timeOptions));
   }, [timeSinceInterval]);
 
 
   useEffect(() => {
-    fetchDataFromPixabay()
+    fetchDataFromPixabay();
   }, [settingsState.photo.searchTerm]);
-
-
 
   return (
     <div className="App">
       <SettingsContext.Provider value={settingsState}>
-        {displayTaskbar ? <Taskbar toggleTaskbar={toggleTaskbar} /> : <Button type={"primary"} text={"Customise"} onClickHandler={toggleTaskbar} />}
+        {displayTaskbar
+          ? <Taskbar toggleTaskbar={toggleTaskbar} />
+          : <Button type={"primary"} text={"Customise"} onClickHandler={toggleTaskbar} />}
         <Clock date={currentDate} time={currentTime} />
         {currentImage && <Background currentImg={currentImage} />}
       </SettingsContext.Provider>
     </div>
   );
-}
+};
 
 export default App;
