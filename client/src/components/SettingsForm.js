b82396b1b8eda172afff6c5ab522f252716c265c
Ross McIntyre <rossmci90@hotmail.co.uk>
2021-04-07 13:34:57 +0100

    dispatch for all time settings function as expected

--- client/src/components/SettingsForm.js
+++ client/src/components/SettingsForm.js
@@ -1,21 +1,39 @@
-import { useSettingsContext } from "../utils/context/SettingsContext"
+import { useSettingsContext } from "../utils/context/SettingsContext";
 
 const SettingsForm = () => {
-  // const [{ timeDisplay }, dispatch] = useSettingsContext();
+
   const [{ timeDisplay }, dispatch] = useSettingsContext();
   return (
     <form>
       <fieldset>
         <legend>Time Display</legend>
         <label for="timeDisplay">Display</label>
-        <input type="checkbox" name="timeDisplay" id="timeDisplay" checked={timeDisplay.show} onChange={() => dispatch({ type: "showTime" })} />
+        <input
+          type="checkbox"
+          name="timeDisplay"
+          id="timeDisplay"
+          checked={timeDisplay.show}
+          onChange={() => dispatch({ type: "showTime" })}
+        />
         <label for="12Hour">12 Hour Format</label>
-        <input type="checkbox" name="12Hour" id="12Hour" checked={timeDisplay.hour12} />
+        <input
+          type="checkbox"
+          name="12Hour"
+          id="12Hour"
+          checked={timeDisplay.hour12}
+          onChange={() => dispatch({ type: "hour12" })}
+        />
         <label for="seconds">Display Seconds</label>
-        <input type="checkbox" name="seconds" id="seconds" checked={timeDisplay.seconds} />
+        <input
+          type="checkbox"
+          name="seconds"
+          id="seconds"
+          checked={timeDisplay.seconds}
+          onChange={() => dispatch({ type: "seconds" })}
+        />
       </fieldset>
     </form>
-  )
-}
+  );
+};
 
-export default SettingsForm
+export default SettingsForm;
--- client/src/utils/context/SettingsContext.js
+++ client/src/utils/context/SettingsContext.js
@@ -4,50 +4,53 @@ const initialState = {
   timeDisplay: {
     show: true,
     hour12: false,
-    seconds: true
+    seconds: true,
   },
   dateDisplay: {
     show: true,
     day: true,
-    format: "long"
+    format: "long",
   },
   font: {
-    color: "#FFFFFF"
+    color: "#FFFFFF",
   },
   photo: {
     interval: 0.25,
-    searchTerm: "landscape"
-  }
-}
+    searchTerm: "landscape",
+  },
+};
 //create a context
-const SettingsContext = createContext()
+const SettingsContext = createContext();
 // destructuer the provider component that is created by createcontext
 const { Provider } = SettingsContext;
 
 //define our reducer callback, here we will add different actions which update state accordingly
 const reducer = (state, action) => {
   //spread existing state, mutate temp object, return the tempstate. We could return a spread but I think this way looks cleaner especially as state is nested
-  const tempState = { ...state }
+  const tempState = { ...state };
   switch (action.type) {
     case "showTime":
-      tempState.timeDisplay.show = !tempState.timeDisplay.show
-      return tempState
-
-
+      tempState.timeDisplay.show = !tempState.timeDisplay.show;
+      return tempState;
+    case "hour12":
+      tempState.timeDisplay.hour12 = !tempState.timeDisplay.hour12;
+      return tempState;
+    case "seconds":
+      tempState.timeDisplay.seconds = !tempState.timeDisplay.seconds;
+      return tempState;
     default:
-      return tempState
+      return tempState;
   }
 };
 //higher order component, gets the initial value of state as a default param
 
 // the example I used spread props, I am not currently show how this operates but it is required for now
-const SettingsProvider = ({
-  value = initialState, ...props }) => {
+const SettingsProvider = ({ value = initialState, ...props }) => {
   const [state, dispatch] = useReducer(reducer, value);
   // the use reducer takes our reduce func and the initial value of state, when dispatch is called it will update state which the whole application has access to by using the custom hook below
-  return <Provider value={[state, dispatch]} {...props} />
+  return <Provider value={[state, dispatch]} {...props} />;
 };
 // custom hook to allow any component access to state and to dispatch (to update state)
-const useSettingsContext = () => useContext(SettingsContext)
+const useSettingsContext = () => useContext(SettingsContext);
 
-export { SettingsProvider, useSettingsContext }
+export { SettingsProvider, useSettingsContext };
