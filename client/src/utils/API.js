0ea2c4e499fa650571470dff9dd1ee58470fbefa
Ross McIntyre <rossmci90@hotmail.co.uk>
2021-04-07 09:21:21 +0100

    added options for localedatestring

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -3,7 +3,7 @@ const API_KEY = process.env.REACT_APP_PIXABAY_API_KEY;
 
 export default {
   getImages: async (searchTerm) => {
-    const results = await axios.get(`https://pixabay.com/api/?key=${API_KEY}&per_page=100&q=${searchTerm}&orientation=horizontal`);
+    const results = await axios.get(`https://pixabay.com/api/?key=${API_KEY}&per_page=100&q=${searchTerm}&orientation=horizontal&image_type=photo`);
     return results
   }
 }
