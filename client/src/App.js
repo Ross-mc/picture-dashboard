89bc1a2bd984b99b2d361aeb3cfd5325e63b8d06
Ross McIntyre <rossmci90@hotmail.co.uk>
2021-04-07 14:49:17 +0100

    if api is not working, a random default img will be displayed temporarily

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,14 @@
 import React, { useEffect, useState, useReducer } from "react";
-import './App.css';
-import { useSettingsContext } from "./utils/context/SettingsContext"
-import Clock from "./components/Clock"
+import "./App.css";
+import { useSettingsContext } from "./utils/context/SettingsContext";
+import Clock from "./components/Clock";
 import Background from "./components/Background";
-import Button from "./components/Button"
-import API from "./utils/API"
+import Button from "./components/Button";
+import API from "./utils/API";
 import Taskbar from "./components/Taskbar";
+import defaultImgs from "./assets/backupImgs";
 
 const App = () => {
-
   const [state] = useSettingsContext();
 
   const [currentDate, setDate] = useState(new Date().toLocaleDateString());
@@ -23,9 +23,10 @@ const App = () => {
     setImage(currentImages[randomNum]);
   };
 
-  const fetchDataFromPixabay = async () => {
-    const responseFromPixabay = await API.getImages(state.photo.searchTerm);
-    setImages(responseFromPixabay.data.hits);
+  const fetchDataFromPixabay = () => {
+    API.getImages(state.photo.searchTerm)
+      .then((res) => setImages(res.data.hits))
+      .catch((err) => console.log(err));
   };
 
   const toggleTaskbar = () => {
@@ -38,8 +39,8 @@ const App = () => {
 
   useEffect(() => {
     setTimeout(() => {
-      setTimeSinceInterval(timeSinceInterval + 1000)
-    }, 1000)
+      setTimeSinceInterval(timeSinceInterval + 1000);
+    }, 1000);
     if (timeSinceInterval % (state.photo.interval * 60 * 1000) === 0) {
       selectRandomImage();
     }
@@ -48,21 +49,21 @@ const App = () => {
     const timeOptions = {
       hour12: timeDisplay.hour12,
       hour: "2-digit",
-      minute: "2-digit"
+      minute: "2-digit",
     };
     if (timeDisplay.seconds) {
       timeOptions.second = "2-digit";
     }
     const dateOptions = {
       month: "long",
-      day: "numeric"
-    }
+      day: "numeric",
+    };
     if (dateDisplay.day) {
-      dateOptions.weekday = "long"
-    };
+      dateOptions.weekday = "long";
+    }
     if (dateDisplay.format === "long") {
-      dateOptions.year = "numeric"
-    };
+      dateOptions.year = "numeric";
+    }
     // tolocaledatestring and localetimestring have two optional arguments. the first defines what locale to use (i.e. en-gb for uk or de-DE for germany) by passing
     // undefined we default to the local computers format
     const date = new Date();
@@ -70,18 +71,24 @@ const App = () => {
     setTime(date.toLocaleTimeString(undefined, timeOptions));
   }, [timeSinceInterval]);
 
-
   useEffect(() => {
     fetchDataFromPixabay();
   }, [state.photo.searchTerm]);
 
   return (
     <div className="App">
-      {displayTaskbar
-        ? <Taskbar toggleTaskbar={toggleTaskbar} />
-        : <Button type={"primary"} text={"Customise"} onClickHandler={toggleTaskbar} />}
+      {displayTaskbar ? (
+        <Taskbar toggleTaskbar={toggleTaskbar} />
+      ) : (
+        <Button
+          type={"primary"}
+          text={"Customise"}
+          onClickHandler={toggleTaskbar}
+        />
+      )}
       <Clock date={currentDate} time={currentTime} />
-      {currentImage && <Background currentImg={currentImage} />}
+      <Background currentImg={currentImage || defaultImgs[Math.floor(Math.random() * defaultImgs.length)]} />
+      
     </div>
   );
 };
---
+++ client/src/assets/backupImgs/index.js
@@ -0,0 +1,5 @@
+import mountainPath from "./mountain-path.jpg";
+
+const defaultImgs = [mountainPath];
+
+export default defaultImgs;
